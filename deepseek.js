cc220b6cd86e30de5829458570737f2f2058bf73
Milad Niroee <mniroee@gmail.com>
2026-05-09 11:21:07 +0330

    add persian text weight evaluating on paragraphs

--- deepseek.js
+++ deepseek.js
@@ -1,3 +1,5 @@
+const PERSIAN_WEIGHT_PERCENTAGE = 60;
+
 function fixDirection() {
   const ignoreElements  = document.querySelectorAll('.katex-html,.md-code-block,code,.ds-virtual-list-items>div>div');
   const paraghraphs = document.querySelectorAll('.ds-markdown-paragraph, h2, h3, h4, .ds-message>div:not(.ds-markdown), ul, ol, table');
@@ -22,26 +24,57 @@ function detectParagraphDirection(text) {
     
     if (text.length === 0) return 'ltr';
     
-    // استفاده از Intl.Segmenter برای شکستن متن به بخش‌های دیداری (گرافِم‌ها)
+    let persianCount = 0;
+    let nonPersianCount = 0;
+
     const segmenter = new Intl.Segmenter('en', { granularity: 'grapheme' });
     const segments = [...segmenter.segment(text)];
     
     for (const segment of segments) {
         const char = segment.segment;
         
+        const isEmojiLike = /[\p{Emoji}\p{Emoji_Presentation}]/u.test(char);
+        if (isEmojiLike) continue;
+        
+        if (char.match(/[\d\s\u200E\u200F\u200B#@$%^&*()\-+=_{}[\]\\|:;"'<>,.?/~`!]/)) continue;
+        
+        const persianRegex = /[\u0600-\u06FF\u0750-\u077F\u08A0-\u08FF\uFB50-\uFDFF\uFE70-\uFEFF]/;
+        
+        let isRtl = persianRegex.test(char);
+        
+        if (isRtl) {
+            persianCount++;
+        } else {
+            nonPersianCount++;
+        }
+    }
+    
+    
+    const totalRelevantChars = persianCount + nonPersianCount;
+    if (totalRelevantChars > 0) {
+        const persianPercentage = (persianCount / totalRelevantChars) * 100;
+        
+        
+        if (persianPercentage > PERSIAN_WEIGHT_PERCENTAGE) {
+            return 'rtl';
+        }
+    }
+    
+    
+    for (const segment of segments) {
+        const char = segment.segment;
         
         const isEmojiLike = /[\p{Emoji}\p{Emoji_Presentation}]/u.test(char);
         if (isEmojiLike) continue;
         
-      
         if (char.match(/[\d\s\u200E\u200F\u200B#@$%^&*()\-+=_{}[\]\\|:;"'<>,.?/~`!]/)) continue;
         
-      
         const persianRegex = /[\u0600-\u06FF\u0750-\u077F\u08A0-\u08FF\uFB50-\uFDFF\uFE70-\uFEFF]/;
+        
         return persianRegex.test(char) ? 'rtl' : 'ltr';
     }
     
-    return 'ltr';
+    return 'ltr'
 }
 
 
